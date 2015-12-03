eceff6939d5744584bb1098692ee9bf7d0ee5806
rbmiao <rbmiao@gmail.com>
2015-12-03 15:31:18 -0500

    update test cases

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -5,5 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
-
+	
+	@Test
+	public void testDebitWithSufficientFunds(){
+		BankAccount account = new BankAccount(100);
+		double amount = account.debit(4);
+		Assert.assertEquals(5.0, amount);
+	}
 }
